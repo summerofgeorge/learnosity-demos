cce7f7e90ea6c75c63f3dcd46de70ab38a14a06d
Michael Sharman <sharmo@gmail.com>
2020-11-06 16:09:06 +1100

    Fixed templates, updated some Spanish thumbnails, using English for the rest

--- www/authoring/item-list-i18n.php
+++ www/authoring/item-list-i18n.php
@@ -9,6 +9,9 @@ include_once 'includes/header.php';
 //common Learnosity config elements including API version control vars
 include_once '../lrn_config.php';
 
+//file/URL util
+include_once 'utils/file.php';
+
 use LearnositySdk\Request\Init;
 use LearnositySdk\Utils\Uuid;
 
@@ -33,26 +36,16 @@ $hasGroupDefaults = ($language === 'ar-EG') ? 'false' : 'true';
 
 /*
     Retrieve the label bundles, per API, that contain translations.
-    We need one for Author API and the embedded Question Editor API
-    (loaded by Author API internally). We store them in separate
-    files for easier maintenance and a cleaner initialization
-    object for this demo file.
+    We need one for Author API, the embedded Question Editor API as
+    well as Questions API (for previewing the Assessment experience).
+    We store them in separate files for easier maintenance and a
+    cleaner initialization object for this demo file.
 */
 if (preg_match('/^[A-Za-z\-]+$/', $language)) {
     $questionsLabels = file_get_contents($baseRepoUrl . $language . '/label_bundles/questions-api.json');
     $authorLabels = file_get_contents($baseRepoUrl . $language . '/label_bundles/author-api.json');
     $questioneditorLabels = file_get_contents($baseRepoUrl . $language . '/label_bundles/questioneditor-api.json');
 }
-
-/*
-    Retrieve the Question Editor custom question templates and groups
-    needed for translated content (like placeholder response options and
-    question stimulus etc).
-    We store them in separate files for easier maintenance and a
-    cleaner initialization object for this demo file.
-*/
-$questionTypeTemplates = file_get_contents($baseRepoUrl . $language . '/qe_custom_types/question_type_templates.json');
-$questionTypeGroups = file_get_contents($baseRepoUrl . $language . '/qe_custom_types/question_type_groups.json');
 
 $security = [
     'consumer_key' => $consumer_key,
@@ -123,8 +116,6 @@ $request = json_decode('{
                         }
                     },
                     "group_defaults": ' . $hasGroupDefaults . ',
-                    "question_type_groups": ' . $questionTypeGroups . ',
-                    "question_type_templates": ' . $questionTypeTemplates . ',
                     "label_bundle": ' . $questioneditorLabels . ',
                     "dependencies": {
                         "questions_api": {
@@ -150,6 +141,26 @@ $request = json_decode('{
     }
 }', true);
 
+/*
+    For demos that have them, retrieve the Question Editor custom question
+    templates and groups needed for translated content (like placeholder
+    response options and question stimulus etc).
+    We store them in separate files for easier maintenance and a cleaner
+    initialization object for this demo file.
+
+    Normally, these would be passed in the standard `request` object, this
+    is a separate step for this demo purely to account for some demos that
+    don't have the templates and/or groups set up.
+*/
+if (urlExists($baseRepoUrl . $language . '/qe_custom_types/question_type_templates.json')) {
+    $questionTypeTemplates = file_get_contents($baseRepoUrl . $language . '/qe_custom_types/question_type_templates.json');
+    $request['config']['dependencies']['question_editor_api']['init_options']['question_type_templates'] = json_decode($questionTypeTemplates, true);
+}
+if (urlExists($baseRepoUrl . $language . '/qe_custom_types/question_type_groups.json')) {
+    $questionTypeGroups = file_get_contents($baseRepoUrl . $language . '/qe_custom_types/question_type_groups.json');
+    $request['config']['dependencies']['question_editor_api']['init_options']['question_type_groups'] = json_decode($questionTypeGroups, true);
+}
+
 $Init = new Init('author', $security, $consumer_secret, $request);
 $signedRequest = $Init->generate();
 ?>
@@ -167,10 +178,16 @@ $signedRequest = $Init->generate();
         <p>This demo uses learnosity-i18n which is a public repository, containing Learnosity internationalization language bundles.</p>
         <p style="margin-bottom:25px;">Click a language icon to see a translation of the assessment below:</p>
         <div>
-            <div class="language-button-container">
+        <div class="language-button-container">
                 <a class="language-button <?php if ($language === 'pt-PT') { echo 'selected'; } ?>" href="/authoring/item-list-i18n.php?language=pt-PT">
                     <img class="language-flag" src="/static/images/i18n/flag-PT.png" />
                     Português / Portuguese
+                </a>
+            </div>
+            <div class="language-button-container">
+                <a class="language-button <?php if ($language === 'es-ES') { echo 'selected'; } ?>" href="/authoring/item-list-i18n.php?language=es-ES">
+                    <img class="language-flag" src="/static/images/i18n/flag-ES.png" />
+                    Español / Spanish
                 </a>
             </div>
             <div class="language-button-container">
